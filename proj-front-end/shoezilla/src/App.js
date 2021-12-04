39d881fd0de04b881d50bd781b1384ce1c222ca7
Arpan Nayankumar Bhatt <ar205025@dal.ca>
2021-12-03 20:36:01 -0400

    Added login and signup pages with Cognito and lambda integration

--- proj-front-end/shoezilla/src/App.js
+++ proj-front-end/shoezilla/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>ShoeZilla</p>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
